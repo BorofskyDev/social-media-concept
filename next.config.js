04ae27d13cf6254d0abcf0225de7985459146698
solovyovsdom <joelborofsky@mac.com>
2021-12-21 14:12:37 -0600

    first commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
+  images: {
+    domains: [
+      "links.papareact.com",
+      "platform-lookaside.fbsbx.com",
+      "firebasestorage.googleapis.com",
+      
+    ],
+  },
+
   reactStrictMode: true,
-}
+
+};
